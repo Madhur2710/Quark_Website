59ef851e14e720aa954ee242808b02b500aa6ba3
[Madhur-Zanwar] <[madhurang.study@gmail.com]>
2025-02-05 20:05:27 +0530

    responsive

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -5,11 +5,11 @@ import Astronaut from "../assets/Astronaut.svg"
 import Background from "../assets/realbg.png"
 export default function home(){
     return(
-        <div className=" w-screen h-screen justify-center ">
+        <div className=" w-screen overflow-hidden h-screen ">
             {/* <video src={glitch}></video> */}
-            <img src={Background} className="absolute w-full h-full z-[-1] "></img>
-            <img src={Quarklogo} className="absolute z-1 mx-[14%] mt-[3%]  "></img>
-            <img src={Astronaut} className="absolute h-[88.6%] mt-[5.4%] mx-[23%]"></img>
+            <img src={Background} draggable="false" className="absolute w-full h-full z-[-1] "></img>
+            <img src={Quarklogo} draggable="false" className="absolute z-1 w-[100%] mt-[3%] md:w-[80%] md:mx-[10%] lg:w-[68%] lg:mx-[16%]  "></img>
+            <img src={Astronaut} draggable="false" className="absolute w-[100%] md:w-[80%] md:mx-[10%] lg:w-[68%] lg:mx-[16%] bottom-[0%]"></img>
         </div>
     )
 }
